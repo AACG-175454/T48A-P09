1b9f97a5e98453d5be59ce675d983497af3dbfa7
AACG-175454 <aleleogb@gmail.com>
2025-10-10 19:19:19 -0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,8 +1,8 @@
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
 from sklearn import metrics
-from src.model import create_model, train_model, predict
-from src.metrics import evaluate_metrics
-
 
 # Cargar datos
 categories = ['alt.atheism', 'comp.graphics', 'sci.space']
@@ -10,28 +10,14 @@ train = fetch_20newsgroups(subset='train', categories=categories)
 test = fetch_20newsgroups(subset='test', categories=categories)
 
 # Crear pipeline
-model = create_model()
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
 # Entrenar
-train_model(model, train.data, train.target)
+model.fit(train.data, train.target)
 
 # Predecir
-predicted = predict(model, test.data)
+predicted = model.predict(test.data)
 
-print("\n Reporte")
-print(metrics.classification_report(test.target, predicted, target_names=test.target_names, zero_division=0))
-
-accuracy = metrics.accuracy_score(test.target, predicted)
-precision_macro, recall_macro, f1_macro = evaluate_metrics(test.target, predicted, average='macro')
-precision_weighted, recall_weighted, f1_weighted = evaluate_metrics(test.target, predicted, average='weighted')
-
-# Evaluar métricas
-precision, recall, f1 = evaluate_metrics(test.target, predicted, average='macro')
-
-print("Métricas de Evaluacion")
-print(f"Precision: {precision:.4f}")
-print(f"Recall: {recall:.4f}")
-print(f"F1-Score: {f1:.4f}")
-print(f"Accuracy (Exactitud): {accuracy:.4f}")
-print(f"Macro Avg -> Precision: {precision_macro:.4f}, Recall: {recall_macro:.4f}, F1: {f1_macro:.4f}")
-print(f"Weighted Avg -> Precision: {precision_weighted:.4f}, Recall: {recall_weighted:.4f}, F1: {f1_weighted:.4f}")
+# Evaluar
+print(metrics.classification_report(test.target, predicted, target_names=test.target_names))
+ 
